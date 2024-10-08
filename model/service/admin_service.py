c7e1915cd6f36910f7447d0e21a9e59d44472bee
nima <nima.mohamad.sanati@gmail.com>
2024-10-08 17:01:40 +0330

    crud repse fixed and made service

---
+++ model/service/admin_service.py
@@ -0,0 +1,30 @@
+from model.entity.admin import Admin
+from model.repository.crud_repository import CrudRepository
+
+
+class AdminService:
+    repo = CrudRepository(Admin)
+
+    @classmethod
+    def save(cls, admin):
+        cls.repo.save(admin)
+
+    @classmethod
+    def edit(cls, admin):
+        cls.repo.edit(admin)
+
+    @classmethod
+    def remove(cls, id):
+        cls.repo.remove(id)
+
+    @classmethod
+    def find_all(cls):
+        return cls.repo.find_all()
+
+    @classmethod
+    def find_by_id(cls, id):
+        return cls.repo.find_by_id(id)
+
+    @classmethod
+    def find_by(cls, by):
+        return cls.repo.find_by(by)
