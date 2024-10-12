8906ac94785fd07688a34fc9d48f46466f18c5a5
Mobina-Sherafat-vaziri <mobinashv2005@gmail.com>
2024-10-12 23:33:36 +0330

    Update salon_service.py

--- model/service/salon_service.py
+++ model/service/salon_service.py
@@ -1,1 +1,36 @@
+from git import repo
+
+from model.entity.salon import Salon
+from model.repository.crud_repository import CrudRepository
+
+class SalonService:
+    repo = CrudRepository(Salon)
+
+    @classmethod
+    def save(cls,salon):
+        cls.repo.save(salon)
+
+    @classmethod
+    def edit(cls,salon):
+        cls.repo.edit(salon)
+
+    @classmethod
+    def remove(cls,salon):
+        cls.repo.remove(salon)
+
+    @classmethod
+    def find_all(cls):
+        cls.repo.find_all()
+
+    @classmethod
+    def find_by_id(cls,id):
+        cls.repo.find_by_id(id)
+
+    @classmethod
+    def find_by_title(cls,title):
+        cls.repo.find_by(Salon._title == title)
+
+    @classmethod
+    def have_capacity(cls,capacity):
+        cls.repo.find_by(Salon._capacity == capacity)
 
