709a96ce2b506d41859749c9fe8198ed7b6d56ac
nastaran akbarian <nastaranakbarian81@gmail.com>
2024-10-13 14:57:22 +0330

    validation

--- model/tools/validation.py
+++ model/tools/validation.py
@@ -1,20 +1,19 @@
 import re
 
 class Validation:
-    pass
-# def no_numbers(prompt,message):
-#     if type(prompt) == str and re.match('^[a-zA-z\s]{3,20}$', prompt):
-#         return prompt
-#     else:
-#         raise ValueError(message)
-#
-# def no_space(prompt,message):
-#     if type(prompt) == str and re.match('^\w{3,20}$', prompt):
-#         return prompt
-#     else:
-#         raise ValueError(message)
-#
-#
+ def no_numbers(prompt,message):
+     if type(prompt) == str and re.match('^[a-zA-z\s]{3,20}$', prompt):
+         return prompt
+     else:
+         raise ValueError(message)
+
+ def no_space(prompt,message):
+     if type(prompt) == str and re.match('^\w{3,20}$', prompt):
+         return prompt
+     else:
+         raise ValueError(message)
+
+
  def password_validator(prompt):
      if type(prompt) == str and re.match(r'^.*(?=a-zA-Z0-9)\w{5,20}$', prompt):
          return prompt
