c277a9f61f78753ccfb591efe3e54d16536fb88f
paniz <abootalebipaniz@gmail.com>
2024-10-13 16:34:11 +0330

    aa

--- controller/event_controller.py
+++ controller/event_controller.py
@@ -1,75 +1,57 @@
-from modle.entity.event import Event
-from modle.service.event_service import EventService
+from model.entity.event import Event
+from model.service.event_service import EventService
+from model.tools.decorators import exception_handling
 
 
 class EventController
 
-    @classmethod
-    def save(cls, title_date, start_time, end_date_time, event_type, duration, description, price):
-        try:
-            event = Event(None, title_date, start_time, end_date_time, event_type, duration, description, price)
-            EventService.save(event)
-            return True, "Event saved"
+@classmethod
+@exception_handling
+def save(cls, title_date, start_time, end_date_time, event_type, duration, description, price):
+        event = Event(None, title_date, start_time, end_date_time, event_type, duration, description, price)
+        EventService.save(event)
+        return "Event saved"
 
-        except Exception as e:
-            return False, str(e)
 
-    @classmethod
-    def edit(cls, id, title_date, start_time, end_date_time, event_type, duration, description, price):
-        try:
-            event = Event(id, title_date, start_time, end_date_time, event_type, duration, description, price)
-            EventService.edit(Event)
-            return True, "Event edited"
-        except Exception as e:
-            return False, str(e)
 
-    @classmethod
-    def remove(cls, id):
-        try:
-            event = Event(id)
-            EventService.remove(event)
-            return True, "Event removed"
-        except Exception as e:
-            return False, str(e)
+@classmethod
+@exception_handling
+def edit(cls, id, title_date, start_time, end_date_time, event_type, duration, description, price):
+        event = Event(id, title_date, start_time, end_date_time, event_type, duration, description, price)
+        EventService.edit(Event)
+        return "Event edited"
 
-    @classmethod
-    def find_all(cls):
-        try:
-            return True, EventService.find_all()
-        except Exception as e:
-            return False, str(e)
 
-    @classmethod
-    def find_by_title(cls, title_date):
-        try:
-            event =  EventService.find_by_title(title_date)
-            if event:
-                return True, event
-            else:
-                return False, "Event not found"
-        except Exception as e:
-            return False, str(e)
+@classmethod
+@exception_handling
+def remove(cls, id):
+        event = Event(id)
+        EventService.remove(event)
+        return True, "Event removed"
 
-    @classmethod
-    def find_by_date_time(cls, start_date_time, end_date_time):
-        try:
-            event =  EventService.find_by_date_time(start_date_time, end_date_time)
-            if event:
-                return True, event
-            else:
-                return False, "Event not found"
-        except Exception as e:
-            return False, str(e)
 
-    @classmethod
-    def find_by_salon(cls, description):
-        try:
-            event =  EventService.find_by_salon(description)
-            if event:
-                return True, event
-            else:
-                return False, "event not fount"
-        except Exception as e:
-            return False, str(e)
+@classmethod
+@exception_handling
+def find_all(cls):
+        return EventService.find_all()
 
-        
+
+@classmethod
+@exception_handling
+def find_by_title(cls, title_date):
+    return EventService.find_by_title(title_date)
+
+@classmethod
+@exception_handling
+def find_by_date_time(cls, start_date_time, end_date_time):
+      return  EventService.find_by_date_time(start_date_time, end_date_time)
+
+
+
+@classmethod
+@exception_handling
+def find_by_salon(cls, description):
+        return  EventService.find_by_salon(description)
+
+
+
