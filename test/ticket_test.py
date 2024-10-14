a30275a8bc972f9d3eab58c92e0d112c06694246
MahsaArya <mahsa3.arya@gmail.com>
2024-10-14 21:48:25 +0330

    Mahsa updated

--- test/ticket_test.py
+++ test/ticket_test.py
@@ -1,3 +1,22 @@
+from datetime import datetime
+
 from model.entity.ticket import Ticket
 from model.service.ticket_service import TicketService
+from controller.ticket_controller import TicketController
 
+from sqlalchemy import create_engine
+from sqlalchemy_utils import create_database, database_exists
+
+from model.entity.base import Base
+
+connection_string = "mysql+pymysql://root:root123@localhost:3306/mft"
+if not database_exists(connection_string):
+    create_database(connection_string)
+
+engine = create_engine(connection_string)
+
+Base.metadata.drop_all(engine)
+Base.metadata.create_all(engine)
+
+start_date = datetime(2024, 11, 12, 20, 17, 30)
+TicketController.save("concert",start_date,90,"concert music",1000000,"Credit Card",111)
