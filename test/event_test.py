630ebe8506083cfbf80c0de006c7f3cf31441a0e
panizabt <abootalebipaniz@gmail.com>
2024-10-14 19:34:38 +0330

    Update event_test.py

--- test/event_test.py
+++ test/event_test.py
@@ -1,6 +1,9 @@
 from controller.event_controller import EventController
+from model.entity.event import Event
+from model.service.event_service import EventService
+
 from datetime import datetime
-from time import time
+
 from sqlalchemy import create_engine
 from sqlalchemy_utils import create_database, database_exists
 
@@ -15,9 +18,7 @@ engine = create_engine(connection_string)
 Base.metadata.drop_all(engine)
 Base.metadata.create_all(engine)
 
-d_t = datetime(2024,5,23,None,None,None)
-s_t = time
-e_t = datetime(None,None,None,20,0,0)
-print (EventController.save( d_t, s_t, e_t,"konserr", "konsert ziba" "2000"
-print (EventController.edit(None
-print (EventController.remove(u
+d_t = datetime(2024,5,23,18, 0, 0)
+s_t = datetime(2024,5,23,18,0,0)
+e_t = datetime(2024,5,23,20,0,0)
+EventController.save(d_t, s_t, e_t, "konsert", "120min", "lotfan 2 saat zod tar hozor peyda konid", "650000")
