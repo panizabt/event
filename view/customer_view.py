2a169461b57b036f7743eecaedb2ce587987d364
nastaranakbarian <nastaranakbarian81@gmail.com>
2024-10-15 14:51:15 +0330

    view

--- view/customer_view.py
+++ view/customer_view.py
@@ -2,31 +2,32 @@ import tkinter
 from tkinter import *
 import tkinter.ttk as ttk
 import tkinter.messagebox as msg
-from controller.lesson_controller import LessonController
+from controller.customer_controller import CustomerController
 from view.component import LabelWithEntry, Table
 
 
-class LessonView:
+class CustomerView:
 
 
     def reset_form(self):
         self.id.set(0)
-        self.title.set("")
-        self.week_day.set("")
-        self.start_date.set(0)
-        self.start_time.set("")
-        self.end_time.set("")
+        self.name.set("")
+        self.family.set("")
+        self.username.set("")
+        self.password.set(0)
+        self.address.set("")
+        self.phone.set("")
+        self.postal_code("")
         #self.clear_table()
         #self.show_on_table()
 
     @staticmethod
-
     def table_click(self, selected_item):
      print(selected_item)
 
 
      def save_click(self):
-        status, message = LessonController.save(self.title.get(), self.week_day.get(), self.start_date.get(), self.start_time.get(),self.end_time.get())
+        status, message = CustomerController.save(self.name.get(), self.family.get(), self.username.get(), self.password.get(),self.address.get(),self.phone.get(),self.postal_code.get())
         if status:
             msg.showinfo("Save",message)
             self.reset_form()
@@ -36,7 +37,7 @@ class LessonView:
 
 
     def edit_click(self):
-        status, message =LessonController.edit(self.id.get(),self.title.get(), self.week_day.get(), self.start_date.get(), self.start_time.get(), self.end_time.get())
+        status, message =CustomerController.edit(self.name.get(), self.family.get(), self.username.get(), self.password.get(),self.address.get(),self.phone.get(),self.postal_code.get())
         if status:
             msg.showinfo("Edit", message)
             self.reset_form()
@@ -46,8 +47,8 @@ class LessonView:
 
 
     def remove_click(self):
-        if msg.askyesno("Remove Lesson","Are you sure you want to remove this lesson?"):
-            status, message =LessonController.remove(self.id.get())
+        if msg.askyesno("Remove person","Are you sure you want to remove this person?"):
+            status, message =CustomerController.remove(self.id.get())
             if status:
                 msg.showinfo("Remove", message)
                 self.reset_form()
@@ -70,7 +71,7 @@ class LessonView:
 
 
         self.table = Table(self.window, ["ID", "Title", "Week_day", "Start_date","End_time"], [60, 100, 100, 80, 80], 250, 20,self.table_click)
-        self.table.refresh_table(LessonController.find_all()[1])
+        self.table.refresh_table(CustomerController.find_all()[1])
 
 
         Button(self.window, text="Save", width=10, command=self.save_click).place(x=100, y=180)
@@ -80,5 +81,5 @@ class LessonView:
         self.reset_form()
         self.window.mainloop()
 
-ui = LessonView()
+ui = CustomerView()
 
