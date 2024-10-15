66a86463cf63fd644b39d07a26b5d930e4d9f6dc
MahsaArya <mahsa3.arya@gmail.com>
2024-10-15 16:49:53 +0330

    updated

--- model/tools/validation.py
+++ model/tools/validation.py
@@ -2,30 +2,30 @@ import re
 from datetime import datetime,date
 
 
-class Validator:
+class Validation:
     @staticmethod
-    def name_validator(cls, name, message):
+    def name_validator( name, message):
         if type(name) ==  str and re.match(r"^[a-zA-Z\s]{2,30}$", name):
             return name
         else:
             raise ValueError(message)
 
     @staticmethod
-    def username_validator(cls, username, message):
+    def username_validator( username, message):
         if type(username) ==  str and re.match(r"^[\w@!#$%^&*\s]{2,30}$", username):
             return username
         else:
             raise ValueError(message)
 
     @staticmethod
-    def password_validator(cls, password, message):
+    def password_validator( password, message):
         if re.match(r"^[\w@!#$%^&*\s]{2,16}$", password):
             return password
         else:
             raise ValueError(message)
 
     @staticmethod
-    def address_validator(cls, address, message):
+    def address_validator(address, message):
         if type(address) == str and re.match(r"^[a-zA-Z\s]{2,30}$", address):
 
 #--------
