fb70bda94f9e6476a9272c5d5e7b8de58fb6ee9f
MahsaArya <mahsa3.arya@gmail.com>
2024-10-20 16:00:26 +0330

    ticket_test.py.py updated

--- test/ticket_test.py
+++ test/ticket_test.py
@@ -22,14 +22,14 @@ start_date = datetime(2024, 11, 12, 20, 17, 30)
 
 TicketController.save("concert", start_date, 90, "concert music", 1000000, "Credit Card", 111)
 
-# TicketController.edit("concert", "concert",  start_date, 90, "Concert music", 1000000, "Credit Card", 111)
+#TicketController.edit("concert", "concert",  start_date, 90, "Concert music", 1000000, "Credit Card", 111)
 
-# TicketController.remove(1)
+#TicketController.remove(2)
 
-# TicketController.find_all()
+#TicketController.find_all()
 
-# TicketController.find_by_id(1)
+#TicketController.find_by_id(1)
 
-# TicketController.find_by_customer(10)
+#TicketController.find_by_customer(10)
 
-# TicketController.find_by_event(123)
+#TicketController.find_by_event(123)
