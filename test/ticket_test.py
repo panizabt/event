ce69a65c4dea494bf0e135e3238be39abc5674da
Messbah.A <Messbah.A@Gmail.com>
2024-10-22 16:02:53 +0330

    messbah updated

--- test/ticket_test.py
+++ test/ticket_test.py
@@ -6,21 +6,16 @@ start_date = datetime(2024, 11, 12, 20, 17, 30)
 # ticket = Ticket(None, "title", start_date, 15, None, 10000, None, None)
 
 d_t = datetime(2024, 11, 12, 20, 17, 30)
-PaymentController.save(10000, d_t, "cardrgfgqreqdf", "Desc")
+_, payment = PaymentController.save(10000, d_t, "cardrgfgqreqdf", "Desc")
 
-_, payment = PaymentController.find_by_id(1)
-
-CustomerController.save("reza", "rezaii", "rez", "reza123", "shargh", 123456788, 12345677)
-_, customer = CustomerController.find_by_id(1)
+_, customer = CustomerController.save("reza", "rezaii", "rez", "reza123", "shargh", 123456788, 12345677)
 
 s_t = datetime(2024,5,23,18,0,0)
 e_t = datetime(2024,5,23,20,0,0)
-EventController.save("Concert ali",  s_t, e_t, "Consert", 120, "lotfan 2 saat zod tar hozor peyda konid", 650000, salon)
-
-_, event = EventController.find_by_id(1)
+_, event  = EventController.save("Concert ali",  s_t, e_t, "Consert", 120, "lotfan 2 saat zod tar hozor peyda konid", 650000, salon)
 
 
-TicketController.save("concert", start_date, 90, event, 1000000, payment, customer)
+_, ticket = TicketController.save("concert", start_date, 90, event, 1000000, payment, customer)
 
 # TicketController.edit(1, "concert", start_date, 90, "Concert music", 1000000, "Credit Card", 111)
 #
