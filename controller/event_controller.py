e48053fb2192f8ed913719c6bf6db2372892727f
paniz <abootalebipaniz@gmail.com>
2024-10-22 16:10:52 +0330

    update

--- controller/event_controller.py
+++ controller/event_controller.py
@@ -33,12 +33,12 @@ class EventController:
     @classmethod
     @exception_handling
     def find_all(cls):
-            return  EventService.find_all()
+            return EventService.find_all()
 
     @classmethod
     @exception_handling
     def find_by_id(cls, id):
-            return  EventService.find_by_id(id)
+            return EventService.find_by_id(id)
 
 
 
@@ -51,7 +51,7 @@ class EventController:
     @classmethod
     @exception_handling
     def find_by_date_time(cls, start_date_time):
-           return  EventService.find_by_date_time(start_date_time)
+           return EventService.find_by_date_time(start_date_time)
 
 
 
