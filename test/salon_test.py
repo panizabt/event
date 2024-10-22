f6125c258be587e1e96cf755ec10ede154ec575c
mobina <mobinashv2005@gmail.com>
2024-10-22 17:16:35 +0330

    view updated

--- test/salon_test.py
+++ test/salon_test.py
@@ -6,6 +6,7 @@ SalonController.save("aria","teh",300,"fgded",True)
 # SalonController.edit()
 # print(SalonController.find_all())
 print(SalonController.find_by_id(1))
+#SalonController.edit("aria","teh",300,"fgded",True)
 # todo : test edit, remove, find_all , find_by_id, find_by_location
 
 SalonView()
