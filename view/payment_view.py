e2b5d741b9c7b5d2a171edfea453dcb1c7019605
RouzbehAB <roozbeh7abadi@gmail.com>
2024-11-07 13:37:51 +0330

    payment veiw has been done !

--- view/payment_view.py
+++ view/payment_view.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from tkinter import *
-import tkinter.ttk as ttk
+# import tkinter.ttk as ttk
 import tkinter.messagebox as msg
 from model.entity import Payment
 from controller.payment_controller import PaymentController
@@ -11,7 +11,7 @@ class PaymentView:
     def reset_form(self):
         self.id.set(0)
         self.amount.set(0)
-        self.date.set("")
+        self.date_time.set("")
         self.payment_type.set("")
         self.description.set("")
 
@@ -28,7 +28,7 @@ class PaymentView:
 
 
     def save_record(self):
-        status, message = PaymentController.save(self.amount.get(), self.date.get(),
+        status, message = PaymentController.save(self.amount.get(), self.date_time.get(),
                                                  self.payment_type.get(), self.description.get())
         if status:
             msg.showinfo("Saved.", message)
@@ -50,7 +50,7 @@ class PaymentView:
 
 
     def edit_record(self):
-        status, message = PaymentController.edit(self.id.get(), self.amount.get(), self.date.get(),
+        status, message = PaymentController.edit(self.id.get(), self.amount.get(), self.date_time.get(),
                                                  self.payment_type.get(), self.description.get())
         if status:
             msg.showinfo("Edited.", message)
@@ -61,7 +61,7 @@ class PaymentView:
 
 
     def find_record(self):
-        status, message = PaymentController.edit(self.id.get(), self.amount.get(), self.date.get(),
+        status, message = PaymentController.edit(self.id.get(), self.amount.get(), self.date_time.get(),
                                                  self.payment_type.get(), self.description.get())
         if status:
             msg.showinfo("found.", message)
@@ -82,26 +82,28 @@ class PaymentView:
 
     def __init__(self):
         win = Tk()
-        win.geometry("1150x450")
+        win.geometry("900x450")
         win.title("Pament Management")
 
-        self.id = LabelWithEntry(win, "Id", 20, 30, data_type="int", state="readonly")
-        self.amount = LabelWithEntry(win, "Amount", 20, 60, data_type="int")
-        self.date = LabelWithEntry(win, "date_time", 20, 100, data_type="date_time")
-        self.payment_type = LabelWithEntry(win, "payment_type", 20, 120, data_type="str")
-        self.description = LabelWithEntry(win, "Description", 20, 140, data_type="str")
+        self.id = LabelWithEntry(win, "Id", 40 , 30, data_type="int", state="readonly")
+        self.amount = LabelWithEntry(win, "Amount", 40, 60, data_type="int")
+        self.date_time = LabelWithEntry(win, "Date_time", 40, 80)
+        self.payment_type = LabelWithEntry(win, "PaymentType", 40, 100, data_type="str")
+        self.description = LabelWithEntry(win, "Description", 40, 120, data_type="str")
 
-        self.table = Table(win, ["Id", "Amount", "Date","payment Type", "description"],[60, 100, 100, 60, 60], 250, 20 ,self.table_click )
+        self.table = Table(win, ["Id", "Amount", "Date","payment Type", "description"],[60, 100, 100, 120, 150], 350, 50 ,self.table_click )
         self.table.refresh_table(PaymentController.find_all()[1])
 
-        self.table = ttk.Treeview(win, columns=(1, 2, 3, 4, 5), show="headings")
-        self.table.place(x=250, y=20)
+        self.show_on_table()
+        # self.table = ttk.Treeview(win, columns=(1, 2, 3, 4, 5), show="headings")
+        # self.table.place(x=250, y=20)
 
 
-        Button(win, text="Save", command=self.save_record).place(x=100, y=220)
-        Button(win, text="Remove", command=self.remove_record).place(x=100, y=250)
-        Button(win, text="Edit", command=self.edit_record).place(x=100, y=280)
-        Button(win, text="Search", command=self.find_record).place(x=100, y=310)
+        Button(win, text="Save", width=10, bg="green", command=self.save_record).place(x=100, y=220)
+        Button(win, text="Remove", width=10, bg="yellow", command=self.remove_record).place(x=100, y=250)
+        Button(win, text="Edit", width=10, bg="red", command=self.edit_record).place(x=100, y=280)
+        Button(win, text="Search", width=10, bg="blue", command=self.find_record).place(x=100, y=310)
+
 
         # self.reset_form()
 
