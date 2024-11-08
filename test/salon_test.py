fa4c61af18cb324ebdae6d358fea3e5dc04af598
Mobina-Sherafat-vaziri <mobinashv2005@gmail.com>
2024-11-08 22:16:43 +0330

    Update salon_test.py

--- test/salon_test.py
+++ test/salon_test.py
@@ -2,11 +2,16 @@ from model.entity import *
 from controller import SalonController
 from view.salon_view import SalonView
 
-SalonController.save("aria","teh",300,"fgded",True)
+SalonController.save("mobina","teh",300,"fgded",True)
 # SalonController.edit()
 # print(SalonController.find_all())
 print(SalonController.find_by_id(1))
-#SalonController.edit("aria","teh",300,"fgded",True)
+#SalonController.edit("mobina","teh",300,"fgded",True)
 # todo : test edit, remove, find_all , find_by_id, find_by_location
+#SalonController.remove(1)
+#SalonController.find_all()
+#SalonController.find_by_id(1)
+#SalonController.find_by_location()
+
 
 SalonView()
